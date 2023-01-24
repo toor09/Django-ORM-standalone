8e56b07c5117d383e584ace21c47f4554e2140cd
Danil Nikitin <d.nikitin.dev@gmail.com>
2023-01-24 20:15:18 +0300

    Output to the console of all passcards.

--- main.py
+++ main.py
@@ -9,4 +9,5 @@ from datacenter.models import Passcard  # noqa: E402
 
 if __name__ == '__main__':
     # Программируем здесь
+    print('Все пропуски:', Passcard.objects.all())  # noqa: T001
     print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
