b07c36459b71b9f35d0684e77a691bbc0c596b04
Danil Nikitin <d.nikitin.dev@gmail.com>
2023-01-24 20:09:33 +0300

    Setting up a database connection.

--- settings.py
+++ settings.py
@@ -1,17 +1,22 @@
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': '',
-        'PORT': '',
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
+        'HOST': env.str("DB_HOST"),
+        'PORT': env.int("DB_PORT"),
+        'NAME': env.str("DB_NAME"),
+        'USER': env.str("DB_USER"),
+        'PASSWORD': env.str("DB_PASSWORD"),
     }
 }
 
 INSTALLED_APPS = ['datacenter']
 
-SECRET_KEY = 'REPLACE_ME'
+SECRET_KEY = env.str("SECRET_KEY")
 
 TIME_ZONE = 'Europe/Moscow'
 
