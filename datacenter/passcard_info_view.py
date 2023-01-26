5a0e7d97a9bdc17b7d4fcb5efc5f2ff2d1af13d9
Danil Nikitin <lfybkf86@mail.ru>
2023-01-26 22:49:30 +0300

    Added handler for non-existent passcode.

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -1,11 +1,12 @@
 from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
 
 def passcard_info_view(request, passcode):
     this_passcard_visits = []
-    passcard = Passcard.objects.get(passcode=passcode)
+    passcard = get_object_or_404(Passcard, passcode=passcode)
     passcard_visits = Visit.objects.filter(passcard__passcode=passcode)
     for passcard_visit in passcard_visits:
         this_passcard_visits.append(
