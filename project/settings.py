2d71aa570a1162cb5b6a347f5f5f44599802ca43
Danil Nikitin <d.nikitin.dev@gmail.com>
2023-01-30 15:05:42 +0300

    Added ALLOWED_HOSTS to .env file.

--- project/settings.py
+++ project/settings.py
@@ -26,8 +26,7 @@ DEBUG = env.bool('DEBUG', default=False)
 
 ROOT_URLCONF = 'project.urls'
 
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATES = [
